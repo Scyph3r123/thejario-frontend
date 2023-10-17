a6bdb1960776488dfdc3613b05e6738a5860072b
Deme <demeyodaniel0@gmail.com>
2023-10-17 16:21:47 +0530

    base path correction

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tr-frontend/',
+  base: '/thejario-frontend/',
   plugins: [react()],
 })
